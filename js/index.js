6ae5b5cb4dc4c7550330af29c8584e7824ec14c7
prince-9999 <princekosta01@gmail.com>
2023-10-02 23:52:57 +0530

    removing file

--- js/index.js
+++
@@ -1,9 +0,0 @@
-function sidebar_open() {
-    document.getElementById("mySidebar").style.display = "flex";
-  }
-  
- function sidebar_close() {
-    document.getElementById("mySidebar").style.display = "none";
-  }
-
-  
