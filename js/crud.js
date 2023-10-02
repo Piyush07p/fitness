41219dc7180ec41acea8b2631163e47d65a4b875
Piyush07p <piyushpatel27978@gmail.com>
2023-10-02 22:39:47 +0530

    firebase functionality added

---
+++ js/crud.js
@@ -0,0 +1,18 @@
+// var query;
+
+// function readQuery(){
+//    query=document.querySelector('.chat_inp textarea').value;
+   
+// }
+
+// let createBtn=document.querySelector('.createBtn')
+// createBtn.addEventListener('click',function(){
+//     readQuery()
+//     firebase.database().ref("queryData/"+Date.now())
+//     .set({
+//         queries:query
+//     })
+//     alert("data inserted")
+//     document.querySelector('.chat_inp textarea').value=""
+    
+// })
---
+++ js/fireConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyCXEdHe1CrCLeRQP4cfdw5OwMiXCFafnOY",
+  authDomain: "fitnessquery-64b66.firebaseapp.com",
+  databaseURL: "https://fitnessquery-64b66-default-rtdb.firebaseio.com",
+  projectId: "fitnessquery-64b66",
+  storageBucket: "fitnessquery-64b66.appspot.com",
+  messagingSenderId: "545468630248",
+  appId: "1:545468630248:web:109c35bed1a8c73efd755d"
+  };
+
+  firebase.initializeApp(firebaseConfig);
